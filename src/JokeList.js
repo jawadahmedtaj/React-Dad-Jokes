ba06318652cee9d6cb1fbc6b1dff777132016db6
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-12-08 18:50:38 +0100

    stylized jokelist comp

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import Axios from "axios";
 
+import "./JokeList.css";
+
 export default class JokeList extends Component {
   static defaultProps = {
     numJokesToGet: 10
@@ -27,7 +29,17 @@ export default class JokeList extends Component {
   render() {
     return (
       <div className="JokeList">
-        <h1>Joke List</h1>
+        <div className="JokeList-Sidebar">
+          <h1 className="JokeList-Title">
+            <span>Dad</span> Jokes
+          </h1>
+          <img
+            src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+            alt=""
+          />
+          <button className="JokesList-Getmore">New Jokes</button>
+        </div>
+
         <div className="JokeList-Jokes">
           {this.state.jokes.map(j => (
             <div>{j}</div>
