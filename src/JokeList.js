fda68a156c03d52f01a037e5f7afc4756fa1708f
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-12-15 15:20:50 +0100

    Loader added

--- src/JokeList.js
+++ src/JokeList.js
@@ -12,7 +12,8 @@ export default class JokeList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]")
+      jokes: JSON.parse(window.localStorage.getItem("jokes") || "[]"),
+      loading: false
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -31,7 +32,8 @@ export default class JokeList extends Component {
     }
     this.setState(
       st => ({
-        jokes: [...st.jokes, ...jokes]
+        jokes: [...st.jokes, ...jokes],
+        loading: false
       }),
       () =>
         window.localStorage.setItem("jokes", JSON.stringify(this.state.jokes))
@@ -51,10 +53,18 @@ export default class JokeList extends Component {
   }
 
   handleClick() {
-    this.getJokes();
+    this.setState({ loading: true }, this.getJokes);
   }
 
   render() {
+    if (this.state.loading) {
+      return (
+        <div className="JokeList-Spinner">
+          <i className="far fa-8x fa-laugh fa-spin"></i>
+          <h1 className="JokeList-Title">Loading...</h1>
+        </div>
+      );
+    }
     return (
       <div className="JokeList">
         <div className="JokeList-Sidebar">
